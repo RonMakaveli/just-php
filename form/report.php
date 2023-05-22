a54eeb84d4fbd56ad091fd910930d4b57acf1f2b
Ronald Gustavo <ronaald_gustavoo@hotmail.com>
2023-05-22 20:03:50 -0300

    adição de variavéis

--- form/report.php
+++ form/report.php
@@ -1,14 +1,20 @@
 <?php
     $when_it_happend = $_POST['whenithappend'];
     $how_long = $_POST['howlong'];
+    $how_many = $_POST['howmany'];
     $alien_description = $_POST['aliendescription'];
+    $what_did = $_POST['whattheydid'];
     $fang_spoted = $_POST['fangspotted'];
+    $other_coment = $_POST['other'];
     $email = $_POST['email'];
 
     echo 'Thanks for submtting the form. <br/>';
     echo 'You were abducted '. $when_it_happend;
     echo ' and were gone for ' . $how_long . '<br/>';
+    echo 'Número de aliens: ' . $how_many . '<br/>';
     echo 'Describe them: ' . $alien_description . '<br/>';
+    echo 'Oque eles fizeram ?' . $what_did . '<br/>';
     echo 'Was fang there ? ' . $fang_spoted . '<br/>';
+    echo 'Outros comentários' . $other_coment . '<br/>';
     echo 'Your email adress is ' . $email;
 ?>
