00b9cd22d96d5f2cfb4965023ebe86ba00222f81
Ronald Gustavo <ronaald_gustavoo@hotmail.com>
2023-05-23 10:28:29 -0300

    criando função mail

--- form/report.php
+++ form/report.php
@@ -8,12 +8,16 @@
     $fang_spoted = $_POST['fangspotted'];
     $other_coment = $_POST['other'];
     $email = $_POST['email'];
-    $msg = "$name foi abduzido $when_it_happend onde ficou por $how_long.\n" .
-    "Descrição do alien: $alien_description\n" .
-    "O que eles fizeram: $what_did\n" .
-    "Identificou meu mano fang: $fang_spoted\n" .
-    "Outros comentários: $other_coment"; 
 
+    $to = 'ronaldgustavo.exp@gmail.com';
+    $subject = 'Reporte de abdução';
+    $msg = "$name foi abduzido $when_it_happend onde ficou por $how_long.\n" .
+        "Descrição do alien: $alien_description\n" .
+        "O que eles fizeram: $what_did\n" .
+        "Identificou meu mano fang: $fang_spoted\n" .
+        "Outros comentários: $other_coment"; 
+    mail($to, $subject, $msg, 'From' . $email);
+                
     echo 'Thanks for submtting the form. <br/>';
     echo 'You were abducted '. $when_it_happend;
     echo ' and were gone for ' . $how_long . '<br/>';
