411ce84d977861373df6019d737d99075318c71a
Ronald Gustavo <ronaald_gustavoo@hotmail.com>
2023-05-23 10:35:31 -0300

    Tradução de mensagens

--- form/report.php
+++ form/report.php
@@ -19,12 +19,12 @@
     mail($to, $subject, $msg, 'From' . $email);
                 
     echo 'Thanks for submtting the form. <br/>';
-    echo 'You were abducted '. $when_it_happend;
-    echo ' and were gone for ' . $how_long . '<br/>';
+    echo 'Quando isso aconteceu ? '. $when_it_happend;
+    echo ' onde ficou por ' . $how_long . '<br/>';
     echo 'Número de aliens: ' . $how_many . '<br/>';
-    echo 'Describe them: ' . $alien_description . '<br/>';
+    echo 'Descreva eles: ' . $alien_description . '<br/>';
     echo 'Oque eles fizeram ? ' . $what_did . '<br/>';
-    echo 'Was fang there ? ' . $fang_spoted . '<br/>';
+    echo 'Viu meu mano fang ? ' . $fang_spoted . '<br/>';
     echo 'Outros comentários: ' . $other_coment . '<br/>';
-    echo 'Your email adress is ' . $email;
+    echo 'Seu endereço de e-mail é ' . $email;
 ?>
